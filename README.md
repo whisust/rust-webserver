9540a0b244e04b4511a405c3a9e7e578a7d3daac
Antoine Latrille <antoine.latrille@gmail.com>
2019-05-26 16:14:17 +0200

    Add hot reload

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Web server written in Rust
 
 The goal is to learn how to do it with actix and dev a first version of an app with a login feature
+
+
+# Hot Reload
+
+Hot reload using systemfd and cargo watch is available for faster development.
+To enable hot reload, start the server with the following command line
+
+`systemfd --no-pid -s http::3000 -- cargo watch -x run`
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate actix_web;
+extern crate listenfd;
+
+use listenfd::ListenFd;
 use actix_web::{server, App, HttpRequest};
 
 fn index(_req: &HttpRequest) -> &'static str {
@@ -6,9 +9,16 @@ fn index(_req: &HttpRequest) -> &'static str {
 }
 
 fn main() {
-    server::new(|| App::new()
-        .resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:8000")
-        .unwrap()
-        .run();
+    let mut listenfd = ListenFd::from_env();
+
+    let mut server = server::new(|| App::new()
+        .resource("/", |r| r.f(index)));
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)
+    } else {
+        server.bind("127.0.0.1:8000").unwrap()
+    };
+
+    server.run();
 }
